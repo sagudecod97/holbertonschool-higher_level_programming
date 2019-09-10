8306e74c803e91a8021a9c8a048353e7d384383d
Santiago <sagudecod97@gmail.com>
2019-09-10 19:58:06 +0000

    Modified If statements, and added elif, and variable neg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if ((number % 10) > 5):
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, (number % 10)))
+neg = 0
+if (number < 0):
+    neg = (number * -1)
+if (neg > 0):
+    print("Last digit of {:d} is -{:d}".format(number, (neg % 10)))
+else:
+    print("Last digit of {:d} is {:d}".format(number, (number % 10)))
 
-if ((number % 10) == 0):
-    print("Last digit of {:d} is {:d} and is 0".format(number, (number % 10)))
-
-if (((number % 10) < 6) & ((number % 10) != 0)):
-    print("The last digit of {:d} is {:d} and is less than 6 and not 0".format(number, (number % 10)))
+if (number > 0 and (number % 10) > 5):
+    print("and is greater than 5")
+elif (((number % 10) == 0) and ((neg % 10) == 0)):
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
