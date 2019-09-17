0581ad5c5a5feb198802ce0dbe1cb5c12c5c99fa
Santiago <sagudecod97@gmail.com>
2019-09-17 20:13:05 +0000

    Added elif in case len 0 for tuples a and b

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,6 +10,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_new = (tuple_a[0], tuple_a[1])
     elif (len(tuple_a) == 0 and not len(tuple_b) == 0):
         tuple_new = (tuple_b[0], tuple_b[1])
+    elif (len(tuple_a) == 0 and len(tuple_b) == 0):
+        tuple_new = (0, 0)
     else:
         tuple_new = ((tuple_a[0] + tuple_b[0]), tuple_a[1] + tuple_b[1])
 
