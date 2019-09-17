33f28b1037789afb7755143420144babcb1ffa06
Santiago <sagudecod97@gmail.com>
2019-09-17 21:17:25 +0000

    Created file 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3#!/usr/bin/python3
+def max_integer(my_list=[]):
+    res = my_list[0]
+    if (len(my_list) ==  0):
+        return None
+
+    for i in range (len(my_list)):
+        if (my_list[i] > res):
+            res = my_list[i]
+
+    return res
