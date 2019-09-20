4ba1098a60a5c43a4812b72b4d50e723d20ffbba
Santiago <sagudecod97@gmail.com>
2019-09-20 01:23:58 +0000

    Modified last If statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
 
     maximum = max(zip(a_dictionary.keys(), a_dictionary.values()))
 
-    if maximum == 0:
+    if maximum is 0:
         return None
 
     return maximum[0]
