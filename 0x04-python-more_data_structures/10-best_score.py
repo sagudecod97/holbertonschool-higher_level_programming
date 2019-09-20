f63f990a6b51c8d2c7befd882ff9d63cf8e6ff73
Santiago <sagudecod97@gmail.com>
2019-09-20 01:19:52 +0000

    Created file 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+
+    maximum = max(zip(a_dictionary.keys(), a_dictionary.values()))
+
+    if maximum == 0:
+        return None
+
+    return maximum[0]
