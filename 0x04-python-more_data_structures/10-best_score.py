5fff277d0758aa53382b03708c94026f542ea93e
Santiago <sagudecod97@gmail.com>
2019-09-20 01:27:10 +0000

    Modified if and zip methods

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,9 @@ def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
 
-    maximum = max(zip(a_dictionary.keys(), a_dictionary.values()))
+    maximum = max(zip(a_dictionary.values(), a_dictionary.keys(),))
 
     if maximum is 0:
         return None
 
-    return maximum[0]
+    return maximum[1]
