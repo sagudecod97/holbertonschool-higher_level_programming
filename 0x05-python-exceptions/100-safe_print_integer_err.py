f40103bcd2740119fd6256799c606ab22f4bd1d3
Santiago <sagudecod97@gmail.com>
2019-09-30 21:03:51 +0000

    Use write instead of print for the except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        #sys.stderr.write(str(e))
-        print(str(e), file=sys.stderr)
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
 
     return True
