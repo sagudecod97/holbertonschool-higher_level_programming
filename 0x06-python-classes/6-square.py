94f913c0182ba93f6418f2e2c629256ec59bdb3b
Santiago <sagudecod97@gmail.com>
2019-10-02 01:17:41 +0000

    Added return in case of Size = 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,6 +39,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+            return
         else:
             for i in range(self.__position[1]):
                 print()
