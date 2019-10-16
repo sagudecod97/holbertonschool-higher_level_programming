5ff6c1f0c6cae41563d1856b8e8ba5bd113ca88f
Santiago <sagudecod97@gmail.com>
2019-10-16 22:54:09 +0000

    Created the files from 10 to 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        i = 0
+
+        if type(attrs) == list:
+            for item in range(len(attrs)):
+                if type(attrs[item]) == str:
+                    i += 1
+
+            if i == len(attrs) and type(attrs) == list:
+                new_dic = {}
+                self_dic = self.__dict__
+
+                for key in self_dic.keys():
+                    if key in attrs:
+                        new_dic[key] = self_dic[key]
+                return new_dic
+        else:
+            return self.__dict__
