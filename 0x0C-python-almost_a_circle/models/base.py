9d2ba2cbcb1a86759f9ed4a67cf64b4ecc2232c6
Santiago <sagudecod97@gmail.com>
2019-10-21 20:04:53 +0000

    Modified files rectangle, base and square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string == None:
+        if json_string is None:
             return []
         return json.loads(json_string)
 
@@ -31,9 +31,39 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         with open("{}.json".format(cls.__name__), 'w+', encoding="utf-8") as f:
+            list_w = []
             if list_objs == None:
                 f.write("[]")
             else:
                 for listObj in list_objs:
-                    json_write = cls.to_json_string([listObj.to_dictionary()])
-                    f.write(json_write)
+                    json_write = listObj.to_dictionary()
+                    list_w.append(json_write)
+
+                f.write(cls.to_json_string(list_w))
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 1, 1, 1)
+        else:
+            dummy = cls(1, 1, 1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        try:
+            with open("{}.json".format(cls.__name__), 'r', encoding="utf-8") as f:
+                list_ret = []
+
+                read_f = f.read()
+                new_f = cls.from_json_string(read_f)
+                for elem in new_f:
+                    elem_app = cls.create(**elem)
+                    list_ret.append(elem_app)
+                return list_ret
+
+        except:
+            print("Hi")
+            return []
