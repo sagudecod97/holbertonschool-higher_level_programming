7d89e414799f8ec58c22292ca25d21c6edfcf431
Santiago <sagudecod97@gmail.com>
2019-10-22 21:33:01 +0000

    Modified initializtion of dummy instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,9 +49,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1, 1, 1)
+            dummy = cls(1, 1)
         else:
-            dummy = cls(1, 1, 1)
+            dummy = cls(1)
 
         dummy.update(**dictionary)
         return dummy
