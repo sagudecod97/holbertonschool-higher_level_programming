cad5114320574672dc519c7d10ccf4b6692016f6
Santiago <sagudecod97@gmail.com>
2019-10-22 19:25:35 +0000

    Modified documentation of Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,12 +3,9 @@
 from models.base import Base
 
 class Rectangle(Base):
-    """Class rectangle definition"""
-
     """
-    Magic Methods
+    Class rectangle definition
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
