ed9582f2e32545e9df76a19c3a6000da4df9dfd6
Santiago <sagudecod97@gmail.com>
2019-10-22 19:23:56 +0000

    Added documentation to class Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" Definition of the class Rectangle, that Inherits from the class Base"""
 from models.base import Base
 
 class Rectangle(Base):
+    """Class rectangle definition"""
 
     """
     Magic Methods
