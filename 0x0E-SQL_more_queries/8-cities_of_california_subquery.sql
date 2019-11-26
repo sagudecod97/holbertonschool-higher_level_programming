5cfd3df817b50bb81e2b0eefac6d26bc454c6bfe
Santiago <sagudecod97@gmail.com>
2019-11-26 20:50:50 +0000

    List all the cities wit state_id of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- List all the cities of California found in the database
+SELECT id, name FROM hbtn_0d_usa.cities WHERE hbtn_0d_usa.cities.state_id = (SELECT hbtn_0d_usa.states WHERE name = 'California');
