fbbfa88e460d934c36b637d3738a0da88412dc3f
Santiago <sagudecod97@gmail.com>
2019-11-26 21:00:18 +0000

    Order by Id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- List all the cities of California found in the database
-SELECT id, name FROM hbtn_0d_usa.cities WHERE hbtn_0d_usa.cities.state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California');
+SELECT id, name FROM hbtn_0d_usa.cities WHERE hbtn_0d_usa.cities.state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY hbtn_0d_usa.cities.id;
