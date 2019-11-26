a21310e485414925b22531e204023da20460ece7
Santiago <sagudecod97@gmail.com>
2019-11-26 22:06:25 +0000

    Fixed Typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- List all cities with id, name and state
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.state_id = state.id ORDER BY cities.id;
+INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
