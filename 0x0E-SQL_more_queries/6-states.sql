d2f9bcb21b9e934e6da56e7a2f028565096af9bf
Santiago <sagudecod97@gmail.com>
2019-11-26 19:37:29 +0000

    Erases NOT NULL in id

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Creates a DB and a table, with an unique id, auto generated and can´t be NULL and
 -- is a primary key, name can't be NULL
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT PRIMARY KEY UNIQUE, name VARCHAR(256) NOT NULL);
