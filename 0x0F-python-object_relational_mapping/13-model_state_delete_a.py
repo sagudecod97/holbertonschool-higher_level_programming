039558a8ccefc4b1665af4ab9d8035738e6c1a42
Santiago <sagudecod97@gmail.com>
2019-12-15 23:42:18 +0000

    Fixed typo in sessiomaker

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,7 +5,7 @@ Deletes all object with name containing 'a'
 
 from sqlalchemy import create_engine
 import sys
-from sqlalchemy.orm import sessionmake
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    Session = sessionmake(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for instance in session.query(State).filter(State.name.like('%a%'))\
