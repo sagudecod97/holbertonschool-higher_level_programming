dc92870714b7e1ce5087913f803b6ee124af5317
Santiago <sagudecod97@gmail.com>
2019-12-15 18:20:25 +0000

    Fixed PEP8 and path to python3 interpreter, and close cursor and db connection

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Cities by States
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    INNER JOIN states ON cities.state_id = states.id;")
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
