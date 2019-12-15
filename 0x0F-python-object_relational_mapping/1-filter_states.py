266240a8be59e115cc7457964890d70237a10a85
Santiago <sagudecod97@gmail.com>
2019-12-15 17:55:00 +0000

    Fixec PEP8, and python3 interpreter path

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python3
-""" List all the states starting by N """
+#!/usr/bin/python3
+"""
+List all the states starting by N
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,  user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
 
     cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id')
