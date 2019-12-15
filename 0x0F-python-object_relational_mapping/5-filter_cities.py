0f2edf18fbdbe038b70492dfd7b4d239c6abfefa
Santiago <sagudecod97@gmail.com>
2019-12-15 18:53:55 +0000

    Fixed PEP8 and path to python3 interpreter, and close cursor and db connection

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+All cities by state
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities WHERE (SELECT states.id FROM states WHERE states.name = %s) = cities.state_id;", (argv[4],))
+    cur.execute("SELECT cities.name FROM cities WHERE (SELECT states.id FROM \
+    states WHERE states.name = %s) = cities.state_id;", (argv[4],))
 
     rows = cur.fetchall()
 
@@ -16,3 +20,6 @@ if __name__ == "__main__":
             print("{:s}, ".format(rows[i][0]), end="")
         else:
             print(rows[i][0])
+
+    cur.close()
+    db.close()
