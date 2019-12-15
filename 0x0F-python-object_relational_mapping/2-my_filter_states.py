391db06f01b3487592066541f5c88021b9f69be4
Santiago <sagudecod97@gmail.com>
2019-12-15 18:06:32 +0000

    Fixed PEP8, python3 interpreter path, and close db, and cursor

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+"""
+Filter states by user input
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name = '{:s}' ORDER BY states.id;".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE states.name = '{:s}' \
+    ORDER BY states.id;".format(argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
