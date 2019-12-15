645d6a4709945ef499d8d4a3012cd5ea30bc9a20
Santiago <sagudecod97@gmail.com>
2019-12-15 18:16:46 +0000

    Fixed PEP8 and path to python3 interpreter, and close cursor and db connection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Prevents SQL injection
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE states.name = %s", (argv[4],))
 
@@ -12,3 +15,6 @@ if __name__ == "__main__":
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
