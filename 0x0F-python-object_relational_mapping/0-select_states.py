e8a8bea0e97cbf66c27062c547119d1094794ad0
Santiago <sagudecod97@gmail.com>
2019-12-15 17:45:04 +0000

    Fixed path of python3, also closed cursor and db connect

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
-""" Lists all the states from the a pass DB """
+#!/usr/bin/python3
+"""
+Lists all the states from the a pass DB
+"""
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1], passwd=argv[2], db=argv[3]);
+    db = MySQLdb.connect(host="127.0.0.1", user=argv[1],
+                        passwd=argv[2], db=argv[3])
 
-    cur = db.cursor();
+    cur = db.cursor()
 
     cur.execute('SELECT * FROM states ORDER BY states.id;')
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
