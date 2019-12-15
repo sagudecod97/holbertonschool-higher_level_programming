14d5e4f97e9cdd25171a233fa1bbec916bacd4df
Santiago <sagudecod97@gmail.com>
2019-12-15 18:01:04 +0000

    Fixed the length of the query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id')
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "N%" \
+    ORDER BY states.id')
     rows = cur.fetchall()
 
     for row in rows:
