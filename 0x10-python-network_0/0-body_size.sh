f3f17f804a10cdd6512977dd22793f038fcc4f7c
Santiago <sagudecod97@gmail.com>
2020-01-07 22:11:44 +0000

    Put everything inside an only line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # Displays the size of the body of the response
-sudo touch  size_body
-curl -so /dev/null "$1" -w '%{size_download}' > size_body
-echo "" >> size_body
-cat size_body
+touch file && curl -so /dev/null "$1" -w '%{size_download}' > file && echo "" >> file && cat file
