66649a249a7c8a0883fa2c601209e02e0c54d44c
Santiago <sagudecod97@gmail.com>
2020-02-04 21:33:58 +0000

    Fixed function declaration

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
+function factorial(num) {
+  let result = 0;
+  if (num === 0) {
+    return (1);
+  } else {
+    result = factorial(num - 1);
+  }
+  return result * num;
+}
+
 if (process.argv.length <= 2) {
   console.log(1);
 } else {
-  const factorial = (num) => {
-    let result = 0;
-    if (num === 0) {
-      return (1);
-    } else {
-      result = factorial(num - 1);
-    }
-    return result * num;
-  };
 
   const argvTwo = process.argv[2];
   const factor = factorial(parseInt(argvTwo));
