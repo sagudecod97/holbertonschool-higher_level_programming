526d72c8ccc89f1866a02a26555b8cc942a0ceaf
Santiago <sagudecod97@gmail.com>
2020-02-04 21:03:21 +0000

    Fixed semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 if (process.argv.length <= 3) {
   console.log('NaN');
 } else {
-  let intOne = parseInt(process.argv[2]);
-  let intTwo = parseInt(process.argv[3]);
+  const intOne = parseInt(process.argv[2]);
+  const intTwo = parseInt(process.argv[3]);
 
   console.log(intOne + intTwo);
 }
