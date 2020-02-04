bfa14caf063dcca28492037c9f9bdb311a4503ea
Santiago <sagudecod97@gmail.com>
2020-02-04 19:25:25 +0000

    Created file that prints three different strings

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let printLanguage = 'C is fun';
+console.log(printLanguage);
+printLanguage = 'Python is cool';
+console.log(printLanguage);
+printLanguage = 'Javascript is amazing';
+console.log(printLanguage);
