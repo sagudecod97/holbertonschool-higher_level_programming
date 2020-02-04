0013c32c753d0919952f922207715d61cc626e37
Santiago <sagudecod97@gmail.com>
2020-02-04 22:14:57 +0000

    Fixed !==

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,11 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let newArr = []
+  const newArr = [];
   const array = process.argv.slice(2, process.argv.length);
   const maxNumber = Math.max(...array);
   for (let i = 0; i < array.length; i++) {
-    if (array[i] != maxNumber) {
+    if (parseInt(array[i]) !== maxNumber) {
       newArr.push(array[i]);
     }
   }
