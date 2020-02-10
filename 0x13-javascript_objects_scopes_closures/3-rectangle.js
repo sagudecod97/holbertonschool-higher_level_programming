d8b1289a3319ac634d0f89e618b12ca3792295c6
Santiago <sagudecod97@gmail.com>
2020-02-10 18:04:40 +0000

    Added method that prints an square

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let string = '';
+      for (let j = 0; j < this.width; j++) {
+        string += 'X';
+      }
+      console.log(string);
+    }
+  }
+}
+
+module.exports = Rectangle;
