0189a8270cd87a0e7ea940618440c33c29424244
Santiago <sagudecod97@gmail.com>
2020-02-10 18:52:56 +0000

    Fixed loop

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,19 +2,14 @@
 const SquareExtends = require('./5-square');
 
 class Square extends SquareExtends {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     let char = c;
     if (char === undefined) {
       char = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.width; i++) {
       let string = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         string += char;
       }
       console.log(string);
