cee8ae3bc187013d741ec08a2eccdd5571702654
Santiago <sagudecod97@gmail.com>
2020-02-10 19:28:19 +0000

    Created function that finds occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+exports.nbOccurences = function (list, searchElement) {
+  let numOccurrences = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      numOccurrences++;
+    }
+  }
+
+  return numOccurrences;
+};
