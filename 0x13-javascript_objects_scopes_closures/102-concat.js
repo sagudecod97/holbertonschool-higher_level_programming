756e1ac112396df270e09b46063781f141877f3d
Santiago <sagudecod97@gmail.com>
2020-02-11 00:50:19 +0000

    Fixed in case of errors

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,14 +3,22 @@ const fs = require('fs');
 const process = require('process');
 const arr = [];
 
-const readA = fs.readFileSync(`./${process.argv[2]}`, { encoding: 'UTF-8' }, (err) => {
-  console.error(err);
+const readA = fs.readFileSync(`./${process.argv[2]}`, (err) => {
+  if (err) {
+    throw err;
+  }
 });
 arr.push(readA);
-const readB = fs.readFileSync(`./${process.argv[3]}`, { encoding: 'UTF-8' }, (err) => {
-  console.error(err);
+const readB = fs.readFileSync(`./${process.argv[3]}`, (err) => {
+  if (err) {
+    throw err;
+  }
 });
 arr.push(readB);
 arr.forEach((read) => {
-  fs.appendFileSync(`./${process.argv[4]}`, read + '\n', { encoding: 'UTF-8' });
+  fs.appendFileSync(`./${process.argv[4]}`, read + '\n', (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 });
